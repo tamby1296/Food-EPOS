7a1d286535bf06aa91fc00fb8069727f905a5746
Tamby Tia RAZAFINDRALAMBO <tambytia.razafindralambo@elca.mu>
2025-08-04 14:19:40 +0400

    add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  resolve: {
+    alias: {
+      "@modules": fileURLToPath(new URL("./src/modules", import.meta.url)),
+      "@components": fileURLToPath(
+        new URL("./src/components", import.meta.url)
+      ),
+      "@icons": fileURLToPath(
+        new URL("./src/assets/icons", import.meta.url + "?react")
+      ),
+    },
+  },
 });
